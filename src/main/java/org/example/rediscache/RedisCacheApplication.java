b820be04969932cb457f95933e1d569dcf38d289
bangjaeyoung <qkdwo4613@gmail.com>
2024-01-08 19:32:36 +0900

    init

---
+++ src/main/java/org/example/rediscache/RedisCacheApplication.java
@@ -0,0 +1,13 @@
+package org.example.rediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCacheApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCacheApplication.class, args);
+    }
+    
+}
