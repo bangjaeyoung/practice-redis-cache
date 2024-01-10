cf1e3749b5e26e0d4ba01bbaec7c8916b9ceba94
bangjaeyoung <qkdwo4613@gmail.com>
2024-01-11 01:26:46 +0900

    RestTemplate 구성에 대한 설명 주석 추가

--- src/main/java/org/example/rediscache/news/config/RestTemplateConfig.java
+++ src/main/java/org/example/rediscache/news/config/RestTemplateConfig.java
@@ -23,7 +23,12 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder
+                // 원본 요청과 응답을 버퍼링하여 여러 번 읽을 수 있도록 설정
+                // 요청 및 응답을 로깅하거나 재사용 가능
                 .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                
+                // UTF-8 인코딩 방식 사용
+                // 문자열 형식의 HTTP 메시지 처리할 수 있도록 설정
                 .additionalMessageConverters(new StringHttpMessageConverter(StandardCharsets.UTF_8))
                 .build();
     }
