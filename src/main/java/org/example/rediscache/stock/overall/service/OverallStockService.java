cc8d78cce04fa86f0b8f70d470d0dc3a8a630f94
bangjaeyoung <qkdwo4613@gmail.com>
2024-01-11 17:11:25 +0900

    전체 주식 데이터 조회 시, 캐시 데이터 저장 - 만료시간 24시간 설정

--- src/main/java/org/example/rediscache/stock/overall/service/OverallStockService.java
+++ src/main/java/org/example/rediscache/stock/overall/service/OverallStockService.java
@@ -1,6 +1,7 @@
 package org.example.rediscache.stock.overall.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.rediscache.exception.BusinessLogicException;
 import org.example.rediscache.exception.ExceptionCode;
 import org.example.rediscache.stock.overall.entity.KOSDAQStockIndex;
@@ -11,10 +12,12 @@ import org.example.rediscache.stock.overall.repository.KOSDAQStockIndexRepositor
 import org.example.rediscache.stock.overall.repository.KOSDAQStockListRepository;
 import org.example.rediscache.stock.overall.repository.KOSPIStockIndexRepository;
 import org.example.rediscache.stock.overall.repository.KOSPIStockListRepository;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -24,16 +27,17 @@ import java.util.stream.Collectors;
  * Date: 2024-01-11
  * Description:
  */
+@Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class OverallStockService {
     
     private final KOSPIStockIndexRepository kospiStockIndexRepository;
     private final KOSDAQStockIndexRepository kosdaqStockIndexRepository;
-    
     private final KOSPIStockListRepository kospiStockListRepository;
     private final KOSDAQStockListRepository kosdaqStockListRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
     
     public List<KOSPIStockIndex> getKOSPIStockIndex() {
         List<KOSPIStockIndex> foundIndices = kospiStockIndexRepository.findAll();
@@ -41,6 +45,27 @@ public class OverallStockService {
         
         String theMostRecentBasDt = foundIndices.get(0).getBasDt();
         List<KOSPIStockIndex> theMostRecentStockIndices = foundIndices.stream().filter(e -> e.getBasDt().equals(theMostRecentBasDt)).collect(Collectors.toList());
+        
+        String cacheKey = "KOSPIStockIndex: ";
+        Object cacheValue = redisTemplate.opsForValue().get(cacheKey);
+        
+        // 캐시 히트일 경우, 저장된 캐시 데이터 반환
+        if (cacheValue != null) {
+            log.info("Redis cache existed!");
+            return (List<KOSPIStockIndex>) cacheValue;
+        }
+        
+        log.info("Redis cache doesn't existed!");
+        
+        // 캐시 미스일 경우, 캐시 데이터 저장
+        redisTemplate.opsForValue()
+                .set(
+                        cacheKey,
+                        theMostRecentStockIndices,
+                        24,
+                        TimeUnit.HOURS
+                );
+        
         
         return theMostRecentStockIndices;
     }
@@ -52,6 +77,26 @@ public class OverallStockService {
         String theMostRecentBasDt = foundIndices.get(0).getBasDt();
         List<KOSDAQStockIndex> theMostRecentStockIndices = foundIndices.stream().filter(e -> e.getBasDt().equals(theMostRecentBasDt)).collect(Collectors.toList());
         
+        String cacheKey = "KOSDAQStockIndex: ";
+        Object cacheValue = redisTemplate.opsForValue().get(cacheKey);
+        
+        // 캐시 히트일 경우, 저장된 캐시 데이터 반환
+        if (cacheValue != null) {
+            log.info("Redis cache existed!");
+            return (List<KOSDAQStockIndex>) cacheValue;
+        }
+        
+        log.info("Redis cache doesn't existed!");
+        
+        // 캐시 미스일 경우, 캐시 데이터 저장
+        redisTemplate.opsForValue()
+                .set(
+                        cacheKey,
+                        theMostRecentStockIndices,
+                        24,
+                        TimeUnit.HOURS
+                );
+        
         return theMostRecentStockIndices;
     }
     
@@ -60,9 +105,29 @@ public class OverallStockService {
         verifyExistsData(foundLists);
         
         String theMostRecentBasDt = foundLists.get(0).getBasDt();
-        List<KOSPIStockList> theMostRecentStockIndices = foundLists.stream().filter(e -> e.getBasDt().equals(theMostRecentBasDt)).collect(Collectors.toList());
+        List<KOSPIStockList> theMostRecentStockLists = foundLists.stream().filter(e -> e.getBasDt().equals(theMostRecentBasDt)).collect(Collectors.toList());
         
-        return theMostRecentStockIndices;
+        String cacheKey = "KOSPIStockList: ";
+        Object cacheValue = redisTemplate.opsForValue().get(cacheKey);
+        
+        // 캐시 히트일 경우, 저장된 캐시 데이터 반환
+        if (cacheValue != null) {
+            log.info("Redis cache existed!");
+            return (List<KOSPIStockList>) cacheValue;
+        }
+        
+        log.info("Redis cache doesn't existed!");
+        
+        // 캐시 미스일 경우, 캐시 데이터 저장
+        redisTemplate.opsForValue()
+                .set(
+                        cacheKey,
+                        theMostRecentStockLists,
+                        24,
+                        TimeUnit.HOURS
+                );
+        
+        return theMostRecentStockLists;
     }
     
     public List<KOSDAQStockList> getKOSDAQStockList() {
@@ -70,9 +135,29 @@ public class OverallStockService {
         verifyExistsData(foundLists);
         
         String theMostRecentBasDt = foundLists.get(0).getBasDt();
-        List<KOSDAQStockList> theMostRecentStockIndices = foundLists.stream().filter(e -> e.getBasDt().equals(theMostRecentBasDt)).collect(Collectors.toList());
+        List<KOSDAQStockList> theMostRecentStockLists = foundLists.stream().filter(e -> e.getBasDt().equals(theMostRecentBasDt)).collect(Collectors.toList());
         
-        return theMostRecentStockIndices;
+        String cacheKey = "KOSDAQStockList: ";
+        Object cacheValue = redisTemplate.opsForValue().get(cacheKey);
+        
+        // 캐시 히트일 경우, 저장된 캐시 데이터 반환
+        if (cacheValue != null) {
+            log.info("Redis cache existed!");
+            return (List<KOSDAQStockList>) cacheValue;
+        }
+        
+        log.info("Redis cache doesn't existed!");
+        
+        // 캐시 미스일 경우, 캐시 데이터 저장
+        redisTemplate.opsForValue()
+                .set(
+                        cacheKey,
+                        theMostRecentStockLists,
+                        24,
+                        TimeUnit.HOURS
+                );
+        
+        return theMostRecentStockLists;
     }
     
     private void verifyExistsData(List<?> data) {
